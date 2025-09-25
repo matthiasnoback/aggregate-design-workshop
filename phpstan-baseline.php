e9ba88de03f6b4a8f0e69333c94bddba0c1b3e12
matthiasnoback <matthiasnoback@gmail.com>
2025-09-25 12:30:45 +0200

    Fixup

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -1,17 +1,23 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
-	'message' => '#^Unreachable statement \\- code above always terminates\\.$#',
-	'identifier' => 'deadCode.unreachable',
-	'count' => 12,
-	'path' => __DIR__ . '/test/Unit/TicketMill/Domain/Model/Concert/ConcertTest.php',
+    'message' => '#^Unreachable statement \\- code above always terminates\\.$#',
+    'identifier' => 'deadCode.unreachable',
+    'count' => 12,
+    'path' => __DIR__ . '/test/Unit/TicketMill/Domain/Model/Concert/ConcertTest.php',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Unreachable statement \\- code above always terminates\\.$#',
-	'identifier' => 'deadCode.unreachable',
-	'count' => 3,
-	'path' => __DIR__ . '/test/Unit/TicketMill/Domain/Model/Reservation/ReservationTest.php',
+    'message' => '#^Unreachable statement \\- code above always terminates\\.$#',
+    'identifier' => 'deadCode.unreachable',
+    'count' => 3,
+    'path' => __DIR__ . '/test/Unit/TicketMill/Domain/Model/Reservation/ReservationTest.php',
 ];
 
-return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
+return [
+    'parameters' => [
+        'ignoreErrors' => $ignoreErrors,
+    ],
+];
